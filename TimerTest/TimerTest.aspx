c7a41d06ba1bc23e6e0e0a712cc0269ca6cc4df0
rbirkmey <47234568+rbirkmey@users.noreply.github.com>
2020-10-18 22:20:43 -0500

    Add files via upload

---
+++ TimerTest/TimerTest.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TimerTest.aspx.cs" Inherits="TimerTest.TimerTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div>
+            <asp:Panel ID="pnlEnterName" runat="server">
+                <label id="nameLabel">Enter Name: </label><asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                <asp:Button ID="btnEnterName" runat="server" Text="Go" OnClick="btnEnterName_Click" />
+            </asp:Panel>
+            
+            <asp:Timer ID="Timer1" runat="server" Interval="500" ontick="Timer1_Tick">
+            </asp:Timer>
+            <asp:UpdatePanel ID="pnlTurns" runat="server" Visible="false">
+                <ContentTemplate>
+                    <asp:Label ID="Label1" runat="server"></asp:Label>
+                    <br />
+                    <asp:Label ID="Label2" runat="server"></asp:Label>
+                    <br />
+                    <asp:Label ID="Label3" runat="server"></asp:Label>
+                    <br />
+                    <asp:Label ID="Label4" runat="server"></asp:Label>
+                    <br />
+                    <asp:Button ID="TurnButton" Text="End Turn" runat="server" Enabled="false" OnClick="TurnButton_Click" />
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="Timer1" EventName="Tick">
+                    </asp:AsyncPostBackTrigger>
+                    <asp:AsyncPostBackTrigger ControlID="TurnButton" EventName="Click">
+                    </asp:AsyncPostBackTrigger>
+                </Triggers>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TimerTest/TimerTest.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimerTest
+{
+    public partial class TimerTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        protected void btnEnterName_Click(object sender, EventArgs e)
+        {
+            // Player class will have instance variables for player name and number
+            // and a bool for if it's their turn, which can be added to a list of players in Application state
+            string playerName = txtName.Text;
+            Session["playerName"] = playerName;
+            int playerNumber = (int)Session["playerNumber"];
+
+            List<bool> isTurn = new List<bool>();
+            isTurn = (List<bool>)Application["isTurn"];
+            int turnPlayer = getTurnPlayer();
+
+            Label3.Text = "You are Player " + playerNumber + ": " + playerName;
+            Label4.Text = "It is currently Player " + turnPlayer + "'s turn";
+
+            if(playerNumber == turnPlayer)
+            {
+                TurnButton.Enabled = true;
+            }
+
+            pnlEnterName.Visible = false;
+            pnlTurns.Visible = true;
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            Label1.Text = DateTime.Now.Second.ToString();
+            Label2.Text = "Number of players online: " + Application["userCount"].ToString();
+            int turnPlayer = getTurnPlayer();
+            Label4.Text = "It is currently Player " + turnPlayer + "'s turn";
+
+            if ((int)Session["playerNumber"] == turnPlayer)
+            {
+                TurnButton.Enabled = true;
+            }
+        }
+
+        protected void TurnButton_Click(object sender, EventArgs e)
+        {
+            List<bool> isTurn = new List<bool>();
+            isTurn = (List<bool>)Application["isTurn"];
+            int turnPlayer = getTurnPlayer();
+            isTurn[turnPlayer - 1] = false;
+            if(isTurn.Count == turnPlayer)
+            {
+                isTurn[0] = true;
+            }
+            else
+            {
+                isTurn[turnPlayer] = true;
+            }
+            Application["isTurn"] = isTurn;
+            TurnButton.Enabled = false;
+        }
+
+        int getTurnPlayer()
+        {
+            List<bool> isTurn = new List<bool>();
+            isTurn = (List<bool>)Application["isTurn"];
+            int turnPlayer = 0;
+
+            for (int x = 0; x < isTurn.Count; x++)
+            {
+                if (isTurn[x] == true)
+                {
+                    turnPlayer = x + 1;
+                }
+            }
+
+            return turnPlayer;
+        }
+    }
+}
